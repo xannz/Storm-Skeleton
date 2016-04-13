ae0562d3c1635ae8fb0d43a78fccf7145a32fd96
sborny <sander.borny@ugent.be>
2016-04-13 04:01:40 -0700

    Added Main class imports, .gitignore

--- src/main/java/io/tengu/storm/Topology.java
+++ src/main/java/io/tengu/storm/Topology.java
@@ -1,9 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.tengu.storm;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
 
 /**
  *
